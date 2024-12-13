c571afea1d63eab02f8a20e76fe7d9c89fe16619
RangkoSenior <chriscoffman90@gmail.com>
2024-12-13 16:32:10 -0600

    add clock

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    BLACK_RGB_SEQUENCE = (0,0,0)
     while True: # start of game loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
-        screen.fill(BLACK_RGB_SEQUENCE) # refresh. should be called last
+                return # exit game loop
+        screen.fill("black")
+        pygame.display.flip()
+        dt += clock.tick(60) / 1000.0
 
 if __name__ == "__main__":
     main()
